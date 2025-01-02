daad6d68b9afda1c14e0cefe545e3d2470bfe864
rcilomba <rcilomba@yahoo.com>
2025-01-02 01:02:31 +0100

    create grid function to change grid size

---
+++ script.js
@@ -0,0 +1,46 @@
+const sketchContainer = document.querySelector("#sketch-container");
+
+function createGrid(size){
+    
+    sketchContainer.innerHTML = "";
+    const squareSize = 960 / size;
+
+    sketchContainer.style.gridTemplateColumns = `repeat(${size}, ${squareSize}px)`; 
+    sketchContainer.style.gridTemplateRows = `repeat(${size}, ${squareSize}px)`;
+
+for(let i = 0; i < size* size; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`; 
+    square.style.height = `${squareSize}px`;
+    sketchContainer.append(square);
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = "black";
+    })
+}
+}
+
+createGrid(16);
+
+
+
+const sizeChangeInput = document.createElement("input");
+sizeChangeInput.placeholder = "Set size: 2-100";
+document.body.insertBefore(sizeChangeInput, document.getElementById("container"));
+
+const sizeChangeButton = document.createElement("button");
+sizeChangeButton.classList.add("size-buttton")
+sizeChangeButton.textContent = "Change size";
+document.body.insertBefore(sizeChangeButton, document.getElementById("container"));
+
+
+sizeChangeButton.addEventListener("click", ()=>{
+    const gridSize = sizeChangeInput.value;
+    console.log("gridsize", gridSize);
+    if(gridSize < 2 || gridSize > 100){
+        alert("Size must be between 2 and 100")
+        return;
+    }
+    createGrid(gridSize);
+})
+
